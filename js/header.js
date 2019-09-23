03a9edd0bce37ea2becfa352069c43e0e0448e9d
Albert <albertkurniawan00@yahoo.com>
2019-09-23 16:04:18 +0700

    Menambahkan daftar dan login page serta modifikasi main page

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@
 
 $('.sub-navbar .dropdown').hover(() => {
     $('.sub-kategori').css('display', 'block');
-    $('.backgroundAll').css('backgroundColor', 'rgba(0, 0, 0, 0.3)');
+    // $('.backgroundAll').css('backgroundColor', 'rgba(0, 0, 0, 0.3)');
 }, () => {
     $('.sub-kategori').css('display', 'none');
     $('.backgroundAll').css('backgroundColor', '#F2F2F2');
@@ -14,7 +14,7 @@ $('.sub-navbar .dropdown').hover(() => {
 
 $('.sub-kategori').hover(() => {
     $('.sub-kategori').css('display', 'block');
-    $('.backgroundAll').css('backgroundColor', 'rgba(0, 0, 0, 0.3)');
+    // $('.backgroundAll').css('backgroundColor', 'rgba(0, 0, 0, 0.3)');
 }, () => {
     $('.sub-kategori').css('display', 'none');
     $('.backgroundAll').css('backgroundColor', '#F2F2F2');
