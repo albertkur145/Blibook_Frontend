ccce3cd982abd08889c5a870eba527f2b4b47ca5
Albert <albertkurniawan00@yahoo.com>
2019-10-02 15:21:02 +0700

    Melanjutkan bag/cart page

---
+++ js/bag-page.js
@@ -0,0 +1,20 @@
+
+let inputJumlahBuku = $("#middle .input-jumlah-buku input[type='number']");
+let jumlah = parseInt(inputJumlahBuku.val());
+
+// tombol plus di klik, increment input number
+$("#middle .input-jumlah-buku div.plus").on('click', () => {
+    jumlah = jumlah + 1;
+
+    inputJumlahBuku.val(jumlah);
+});
+
+
+
+// tombol minus di klik, decrement input number
+$("#middle .input-jumlah-buku div.minus").on('click', () => {
+    if(jumlah > 0)
+        jumlah = jumlah - 1;
+
+    inputJumlahBuku.val(jumlah);
+});
