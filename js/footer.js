b350166ddb0a0d62b1d0bac3333bb54ffeb7224d
Albert <albertkurniawan00@yahoo.com>
2019-10-09 01:00:51 +0700

    Membenarkan css mobile first untuk header dan footer

---
+++ js/footer.js
@@ -0,0 +1,52 @@
+// responsive - resize window
+
+$(window).ready(responsiveSize);
+$(window).resize(responsiveSize);
+
+
+
+function responsiveSize() {
+    let width = $(window).width();
+    
+    const colFooter = $('#footer .row');
+    
+
+    if (width < 465) {
+        
+        if (colFooter.find('.col-3')) {
+            $('.col-3', colFooter).addClass('col-sm-6');
+            $('.col-3', colFooter).removeClass('col-3');
+        }
+
+        if (colFooter.find('.col-6')) {
+            $('.col-6', colFooter).addClass('col-sm-6');
+            $('.col-6', colFooter).removeClass('col-6');
+        }
+
+    } else if (width < 977) {
+
+        if(colFooter.find('.col-3')) {
+            $('.col-3', colFooter).addClass('col-6');
+            $('.col-3', colFooter).removeClass('col-3');
+        }
+
+        if (colFooter.find('.col-sm-6')) {
+            $('.col-sm-6', colFooter).addClass('col-6');
+            $('.col-sm-6', colFooter).removeClass('col-sm-6');
+        }
+
+    }  else if (width > 977) {
+
+        if (colFooter.find('.col-6')) {
+            $('.col-6', colFooter).addClass('col-3');
+            $('.col-6', colFooter).removeClass('col-6');
+        }
+
+        if (colFooter.find('.col-sm-6')) {
+            $('.col-sm-6', colFooter).addClass('col-3');
+            $('.col-sm-6', colFooter).removeClass('col-sm-6');
+        }
+
+    }
+
+}
