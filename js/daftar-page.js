07cfe5726bee37ae5b2e7e0672e0c4e972e3c8ae
Albert <albertkurniawan00@yahoo.com>
2019-10-10 00:17:39 +0700

    Membenarkan css mobile first di daftar dan login page

---
+++ js/daftar-page.js
@@ -0,0 +1,54 @@
+// responsive - resize window
+
+$(window).ready(responsiveSize);
+$(window).resize(responsiveSize);
+
+function responsiveSize() {
+    let width = $(window).width();
+
+    const left = $('#content .left');
+    const right = $('#content .right');
+
+
+
+    if (width < 544) {
+        if (left.find('col-6'))
+            left.css('display', 'none');
+
+        if (right.find('col-6')) {
+            right.addClass('col-12');
+            right.removeClass('col-6');
+        }
+
+
+        $('h2', right).css('textAlign', 'center');
+
+    } else if (width < 1185) {
+        if (right.find('col-12')) {
+            right.addClass('col-6');
+            right.removeClass('col-12');
+        }
+
+
+        $('h2', right).css('textAlign', 'left');
+        left.css('display', 'block');
+
+        $('hr', left).css('width', '67.77777%');
+        $('hr', left).css('top', '9.33609%');
+        
+    } else if (width > 1185) {
+        if (right.find('col-12')) {
+            right.addClass('col-6');
+            right.removeClass('col-12');
+        }
+
+
+        $('h2', right).css('textAlign', 'left');
+        left.css('display', 'block');
+
+        $('hr', left).css('width', '56.48148%');
+        $('hr', left).css('top', '9.22131%');
+    }
+}
+
+// responsive - resize window
