ba2d6701931306018efd08ae85640533aa939751
Albert <albertkurniawan00@yahoo.com>
2019-10-20 00:56:53 +0700

    Menambahkan css mobile first di wishlist page

--- js/wishlist-page.js
+++ js/wishlist-page.js
@@ -6,6 +6,8 @@ let deskBuku = $('.desk-buku p.deskripsi', buku);
 let textJudul = [];
 let textDeskripsi = [];
 
+console.log(judulBuku);
+
 $.each(judulBuku, (key, value) => {
     textJudul.push(value.innerHTML);
 });
@@ -33,9 +35,79 @@ function responsiveSize() {
     let width = $(window).width();
     const wish = $('#content .right .rbody .wishlist');
     const image = $('.image-buku', wish);
+    const desk = $('.desk-buku', wish);
+    const btnBeli = $('.button-beli', wish);
 
 
-    if (width < 945) {
+    if (width < 436) {
+
+        // if (image.hasClass('col-3')) {
+        //     image.addClass('col-12');
+        //     image.removeClass('col-3');
+        // }
+
+        // if (desk.hasClass('col-6')) {
+        //     desk.addClass('col-12');
+        //     desk.removeClass('col-6');
+        // }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 10) {
+                let temp = textJudul[i].substring(0, 10) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 52) {
+                let temp = textDeskripsi[i].substring(0, 52) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+    }
+
+    else if (width < 465) {
+        $('img', image).css('width', '8.125rem');
+        $('img', image).css('height', '13.125rem');
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 11) {
+                let temp = textJudul[i].substring(0, 11) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 67) {
+                let temp = textDeskripsi[i].substring(0, 67) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+    }
+    
+    else if (width < 520) {
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 9) {
+                let temp = textJudul[i].substring(0, 9) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+    }
+
+
+    else if (width < 753) {
+        $('img', image).css('width', '7.5rem');
+        $('img', image).css('height', '11.875rem');
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 12) {
+                let temp = textJudul[i].substring(0, 12) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+    }
+
+    else if (width < 945) {
         $('img', image).css('width', '9.375rem');
         $('img', image).css('height', '14.375rem');
 
