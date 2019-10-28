03ed900eceab13f74b5dbd265b6e9d79e602333c
Albert <albertkurniawan00@yahoo.com>
2019-10-28 20:31:39 +0700

    Menambahkan search page

---
+++ js/search-page.js
@@ -0,0 +1,464 @@
+
+// ambil data json
+
+const buku = $('#pencarian-buku .items-search .card-buku');
+let judulBuku = $('h5.judul-buku', buku);
+let deskBuku = $('p.desk-buku', buku);
+
+let textJudul = [];
+let textBuku = [];
+
+$.each(judulBuku, (key, value) => {
+    textJudul.push(value.innerHTML);
+});
+
+$.each(deskBuku, (key, value) => {
+    textBuku.push(value.innerHTML);
+});
+
+// ambil data json
+
+
+
+
+$(window).ready(responsiveSize);
+$(window).resize(responsiveSize);
+
+
+function responsiveSize() {
+    let width = $(window).width();
+    const itemsBuku = $('#pencarian-buku .items-search .row');
+
+
+    if (width < 300) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-12');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-12');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-12');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 37) {
+                let temp = textJudul[i].substring(0, 37) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 137) {
+                let temp = textBuku[i].substring(0, 137) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 320) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-12');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-12');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-12');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '75%');
+        $('.card-buku', itemsBuku).css('marginLeft', '15%');
+        $('.card-buku img', itemsBuku).css('height', '55%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 27) {
+                let temp = textJudul[i].substring(0, 27) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 127) {
+                let temp = textBuku[i].substring(0, 127) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 345) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-12');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-12');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-12');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '75%');
+        $('.card-buku', itemsBuku).css('marginLeft', '15%');
+        $('.card-buku img', itemsBuku).css('height', '55%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 37) {
+                let temp = textJudul[i].substring(0, 37) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 132) {
+                let temp = textBuku[i].substring(0, 132) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 385) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-12');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-12');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-12');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '75%');
+        $('.card-buku', itemsBuku).css('marginLeft', '15%');
+        $('.card-buku img', itemsBuku).css('height', '55%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 40) {
+                let temp = textJudul[i].substring(0, 40) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 207) {
+                let temp = textBuku[i].substring(0, 207) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 400) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-6');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-6');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-6');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '105%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '50%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 10) {
+                let temp = textJudul[i].substring(0, 10) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 59) {
+                let temp = textBuku[i].substring(0, 59) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 420) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-6');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-6');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-6');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '50%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 11) {
+                let temp = textJudul[i].substring(0, 11) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 67) {
+                let temp = textBuku[i].substring(0, 67) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 440) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-6');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-6');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-6');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '53%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 12) {
+                let temp = textJudul[i].substring(0, 12) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 87) {
+                let temp = textBuku[i].substring(0, 87) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 465) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-6');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-6');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-6');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '55%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 14) {
+                let temp = textJudul[i].substring(0, 14) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 120) {
+                let temp = textBuku[i].substring(0, 120) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 753) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-6');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-6');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-6');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '57%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 17) {
+                let temp = textJudul[i].substring(0, 17) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 143) {
+                let temp = textBuku[i].substring(0, 143) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 977) {
+
+        if (itemsBuku.find('.col-sm-3')) {
+            $('.col-sm-3', itemsBuku).addClass('col-sm-4');
+            $('.col-sm-3', itemsBuku).removeClass('col-sm-3');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-sm-4');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-sm-4');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '56%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 15) {
+                let temp = textJudul[i].substring(0, 15) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 133) {
+                let temp = textBuku[i].substring(0, 133) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width < 1185) {
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-sm-3');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-sm-3');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-sm-3');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '56%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 12) {
+                let temp = textJudul[i].substring(0, 12) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 137) {
+                let temp = textBuku[i].substring(0, 137) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+    } else if (width >= 1185) {
+
+        if (itemsBuku.find('.col-sm-4')) {
+            $('.col-sm-4', itemsBuku).addClass('col-sm-3');
+            $('.col-sm-4', itemsBuku).removeClass('col-sm-4');
+        }
+
+        if (itemsBuku.find('.col-6')) {
+            $('.col-6', itemsBuku).addClass('col-sm-3');
+            $('.col-6', itemsBuku).removeClass('col-6');
+        }
+
+        if (itemsBuku.find('.col-12')) {
+            $('.col-12', itemsBuku).addClass('col-sm-3');
+            $('.col-12', itemsBuku).removeClass('col-12');
+        }
+
+        $('.card-buku', itemsBuku).css('width', '100%');
+        $('.card-buku', itemsBuku).css('marginLeft', '0');
+        $('.card-buku img', itemsBuku).css('height', '60%');
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length >= 17) {
+                let temp = textJudul[i].substring(0, 17) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textBuku.length; i++) {
+            if (textBuku[i].length >= 263) {
+                let temp = textBuku[i].substring(0, 263) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+    }
+}
