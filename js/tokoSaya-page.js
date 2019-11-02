2bdc20d9447cc43075a0cd764d594e832ab295b0
Albert <albertkurniawan00@yahoo.com>
2019-11-02 21:48:47 +0700

    Menyelesaikan css mobile first toko saya page

--- js/tokoSaya-page.js
+++ js/tokoSaya-page.js
@@ -33,7 +33,6 @@ function DOM_Manipulation() {
 // responsive - resize window
 function responsiveSize() {
     let width = $(window).width();
-    $('.angka').html(width);
 
     const buku = $('#content .right .rbody .penjualan .buku');
     const image = $('.image-buku', buku);
@@ -41,7 +40,87 @@ function responsiveSize() {
     const btnDetail = $('.button-detail', buku);
 
 
-    if (width < 520) {
+    if (width < 275) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-12');
+            image.removeClass('col-3');
+
+            desk.addClass('col-12');
+            desk.removeClass('col-7');
+
+            btnDetail.addClass('col-12');
+            btnDetail.removeClass('col-2');
+        } else if (image.hasClass('col-4')) {
+            image.addClass('col-12');
+            image.removeClass('col-4');
+
+            desk.addClass('col-12');
+            desk.removeClass('col-6');
+
+            btnDetail.addClass('col-12');
+            btnDetail.removeClass('col-2');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 18) {
+                let temp = textJudul[i].substring(0, 18) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 197) {
+                let temp = textDeskripsi[i].substring(0, 197) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '100%');
+        $('img', image).css('height', '100%');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 305) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-12');
+            image.removeClass('col-3');
+
+            desk.addClass('col-12');
+            desk.removeClass('col-7');
+
+            btnDetail.addClass('col-12');
+            btnDetail.removeClass('col-2');
+        } else if (image.hasClass('col-4')) {
+            image.addClass('col-12');
+            image.removeClass('col-4');
+
+            desk.addClass('col-12');
+            desk.removeClass('col-6');
+
+            btnDetail.addClass('col-12');
+            btnDetail.removeClass('col-2');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 22) {
+                let temp = textJudul[i].substring(0, 22) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 207) {
+                let temp = textDeskripsi[i].substring(0, 207) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '100%');
+        $('img', image).css('height', '100%');        
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 330) {
 
         if (image.hasClass('col-3')) {
             image.addClass('col-4');
@@ -49,11 +128,310 @@ function responsiveSize() {
 
             desk.addClass('col-6');
             desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
         }
 
         for (let i = 0; i < textJudul.length; i++) {
-            if (textJudul[i].length > 24) {
-                let temp = textJudul[i].substring(0, 24) + "...";
+            if (textJudul[i].length > 19) {
+                let temp = textJudul[i].substring(0, 19) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '5.625rem');
+        $('img', image).css('height', '8.4375rem');
+        $('p.deskripsi', desk).css('display', 'none');
+
+    } else if (width < 355) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 12) {
+                let temp = textJudul[i].substring(0, 12) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 29) {
+                let temp = textDeskripsi[i].substring(0, 29) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '6.25rem');
+        $('img', image).css('height', '10.0625rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 365) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 13) {
+                let temp = textJudul[i].substring(0, 13) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 32) {
+                let temp = textDeskripsi[i].substring(0, 32) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '6.375rem');
+        $('img', image).css('height', '10.125rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 385) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 14) {
+                let temp = textJudul[i].substring(0, 14) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 37) {
+                let temp = textDeskripsi[i].substring(0, 37) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '6.375rem');
+        $('img', image).css('height', '10.125rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 405) { 
+    
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 15) {
+                let temp = textJudul[i].substring(0, 15) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 67) {
+                let temp = textDeskripsi[i].substring(0, 67) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '7.125rem');
+        $('img', image).css('height', '11.25rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 425) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 17) {
+                let temp = textJudul[i].substring(0, 17) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 72) {
+                let temp = textDeskripsi[i].substring(0, 72) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '7.125rem');
+        $('img', image).css('height', '11.25rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 436) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 17) {
+                let temp = textJudul[i].substring(0, 17) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 97) {
+                let temp = textDeskripsi[i].substring(0, 97) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '7.75rem');
+        $('img', image).css('height', '12.4375rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 465) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 18) {
+                let temp = textJudul[i].substring(0, 18) + "...";
+                judulBuku[i].innerHTML = temp;
+            }
+        }
+
+        for (let i = 0; i < textDeskripsi.length; i++) {
+            if (textDeskripsi[i].length > 103) {
+                let temp = textDeskripsi[i].substring(0, 103) + "...";
+                deskBuku[i].innerHTML = temp;
+            }
+        }
+
+        $('img', image).css('width', '8rem');
+        $('img', image).css('height', '12.5rem');
+        $('p.deskripsi', desk).css('display', 'block');
+
+    } else if (width < 520) {
+
+        if (image.hasClass('col-3')) {
+            image.addClass('col-4');
+            image.removeClass('col-3');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
+        }
+
+        for (let i = 0; i < textJudul.length; i++) {
+            if (textJudul[i].length > 19) {
+                let temp = textJudul[i].substring(0, 19) + "...";
                 judulBuku[i].innerHTML = temp;
             }
         }
@@ -70,11 +448,20 @@ function responsiveSize() {
 
             desk.addClass('col-6');
             desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
         }
 
         for (let i = 0; i < textJudul.length; i++) {
-            if (textJudul[i].length > 33) {
-                let temp = textJudul[i].substring(0, 33) + "...";
+            if (textJudul[i].length > 21) {
+                let temp = textJudul[i].substring(0, 21) + "...";
                 judulBuku[i].innerHTML = temp;
             }
         }
@@ -91,11 +478,20 @@ function responsiveSize() {
 
             desk.addClass('col-6');
             desk.removeClass('col-7');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
         }
 
         for (let i = 0; i < textJudul.length; i++) {
-            if (textJudul[i].length > 24) {
-                let temp = textJudul[i].substring(0, 24) + "...";
+            if (textJudul[i].length > 20) {
+                let temp = textJudul[i].substring(0, 20) + "...";
                 judulBuku[i].innerHTML = temp;
             }
         }
@@ -119,6 +515,15 @@ function responsiveSize() {
 
             desk.addClass('col-7');
             desk.removeClass('col-6');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
         }
 
         for (let i = 0; i < textJudul.length; i++) {
@@ -147,6 +552,15 @@ function responsiveSize() {
 
             desk.addClass('col-7');
             desk.removeClass('col-6');
+        } else if (image.hasClass('col-12')) {
+            image.addClass('col-4');
+            image.removeClass('col-12');
+
+            desk.addClass('col-6');
+            desk.removeClass('col-12');
+
+            btnDetail.addClass('col-2');
+            btnDetail.removeClass('col-12');
         }
         
         for (let i = 0; i < textJudul.length; i++) {
