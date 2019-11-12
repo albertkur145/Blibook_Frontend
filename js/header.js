8e0935c5dc5d3143c58611c3bb35d472d3303cae
Albert <albertkurniawan00@yahoo.com>
2019-11-12 11:27:05 +0700

    Menambahkan option user setelah login

--- js/header.js
+++ js/header.js
@@ -1,21 +1,60 @@
 
 
+// variabel global
+const optUser = $('.nav-blibuku .user');
+let namaUser = optUser.text();
+    
+
 // batasan length string nama
 function constraintNama() {
-    const optUser = $('.nav-blibuku .option-user');
-    let namaUser = optUser.text();
-
+    let tempNama;
     if (namaUser.length > 13) 
-        namaUser = " " + namaUser.substring(0, 13) + "... ";
+        tempNama = namaUser.substring(0, 13) + "... ";
 
     optUser.html(`
-        <i class = "fas fa-user"></i>`+ namaUser +`
+        <i class = "fas fa-user"></i> ${tempNama}
         <span><i class="fas fa-chevron-down"></i></span>
     `);
 }
 
 
+// hover option user
+$('.nav-blibuku .user').hover(() => {
+    $('.bg-hover').css('opacity', '1');
+    $('.bg-hover').css('zIndex', '998');
+}, () => {
+    $('.bg-hover').css('opacity', '0');
+    $('.bg-hover').css('zIndex', '-999');
+});
+
+$('.nav-blibuku .option').hover(() => {
+    $('.bg-hover').css('opacity', '1');
+    $('.bg-hover').css('zIndex', '998');
+}, () => {
+    $('.bg-hover').css('opacity', '0');
+    $('.bg-hover').css('zIndex', '-999');
+});
+
+
+
+// responsive - resize window
+function responsiveSize() {
+    let width = $(window).width();
+
+    if (width < 945) {
+        optUser.html(`
+            <i class = "fas fa-user"></i> ${namaUser}
+        `);
+    }
+    else if (width >= 945) {
+        constraintNama();
+    }
+}
+
+
 // document ready
-constraintNama();
+responsiveSize();
+$(window).resize(responsiveSize);
+
 
 
